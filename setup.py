7b55cef1ae84c0514df1e0c886966ab65f16d863
Serhii Budnikov <62923860+serjbuda@users.noreply.github.com>
2023-04-10 14:33:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ setup(
     name='my_package',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'tkinter',
-    ],
+    install_requires=[],
     entry_points={
         'console_scripts': [
             'bot_helper=my_package.bot_interface:run'
